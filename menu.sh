7455a44e44923a477a726d27f16418db61d91c80
Yago <a22yagodj@iessanclemente.net>
2023-01-18 12:46:26 +0100

    Primer commit

---
+++ menu.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+echo `clear` 
+
+function caso1() {
+    echo "Tu nombre actual es: `whoami`"
+    read -p "Introduce el nuevo nombre: " nombre
+    echo "Tu nuevo Nombre es: $nombre"
+}
+
+
+function caso2() {
+    for file in $HOME/*
+    do 
+        if [ -d $file ]
+        then
+            echo "Es un directorio" $file
+
+        else
+            echo "Es un archivo" $file
+        fi
+    done
+}
+
+function caso3() {
+    fecha=`date`
+    echo "La fecha actual es: " $fecha
+    read -p "Escribe la nueva fecha: " nueva
+    echo "La nueva fecha es: " $nueva
+ 
+}
+
+function caso4() {
+    read -p "Valor 1: " x
+    read -p "Valor 2: " y
+    echo La suma es: $(( x + y )) 
+}
+
+seleccion=0
+
+while (( $seleccion != 6 ))
+do
+echo "-Pulsa 1 para cambiar el nombre de usuario "
+echo "-Pulsa 2 para imprimir el directorio home" 
+echo "-Pulsa 3 para cambiar la fecha"
+echo "-Pulsa 4 para sumar dos valores"
+echo "-Pulsa 5 para salir"
+read seleccion
+case $seleccion in
+    1)
+    caso1
+    ;;
+    2)
+    caso2
+    ;;
+    3)
+    caso3
+    ;;
+    4)
+    caso4
+    ;;
+    5)
+    seleccion=6
+
+esac
+done
+
+
